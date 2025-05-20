79714d74e568357eac1272e316155a90acaa63c6
Andrei Panov <andrew.panov@onlyoffice.com>
2025-05-20 13:55:08 +0300

    feat(server): additional logs to tenant parser

--- server/ASC.ZoomService/Middlewares/ParseTenantMiddleware.cs
+++ server/ASC.ZoomService/Middlewares/ParseTenantMiddleware.cs
@@ -35,18 +35,19 @@ namespace ASC.ZoomService.Middlewares
             }
         }
 
-        private static async Task<bool> TryParseFromDomain(string domain, HttpContext httpContent, TenantManager tenantManager, ILogger<ParseTenantHubFilter> logger)
+        private static async Task<bool> TryParseFromDomain(string domainIn, HttpContext httpContent, TenantManager tenantManager, ILogger<ParseTenantHubFilter> logger)
         {
-            domain = domain.Replace(".", @"\.");
+            var domain = domainIn.Replace(".", @"\.");
 
             var regex = new Regex($@"http[s]{{0,1}}:\/\/([a-z\-0-9]+)\.{domain}");
             var uri = httpContent.Request.Url().AbsoluteUri;
-            logger.LogDebug($"Current tenant is null, trying to find one from host {uri} using {domain} as base domain");
+            logger.LogDebug($"Current tenant is null, trying to find one from uri {uri} using {domainIn} as base domain");
 
             Match match = regex.Match(uri);
             if (match.Success)
             {
                 var tenantAlias = match.Groups[1].Value;
+                logger.LogDebug($"Found tenant alias {tenantAlias}");
 
                 var hostedSolution = httpContent.RequestServices.GetService<HostedSolution>();
                 var tenant = await hostedSolution.GetTenantAsync(tenantAlias);
@@ -54,6 +55,7 @@ namespace ASC.ZoomService.Middlewares
                 tenantManager.SetCurrentTenant(tenant);
                 return true;
             }
+            logger.LogDebug("No regex match found");
             return false;
         }
     }
